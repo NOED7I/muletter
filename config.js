9cff328d4c8fb61b2fe208d9aee4ab8ca9db85a1
Kim Sible <kimsible@users.noreply.github.com>
2017-01-24 16:02:04 +0100

    fix heroku start

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	port: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-	host: process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+	host: process.env.OPENSHIFT_NODEJS_IP || null,
   title: 'MuList',
 }
